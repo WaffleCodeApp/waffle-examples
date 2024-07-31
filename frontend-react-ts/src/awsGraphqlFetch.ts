30c319377d17bde87b0eaa7563db79aff063ae6b
Gergely Borgulya <gergely@borgulya.hu>
2024-07-31 03:17:53 +0000

    backend url fix

--- frontend-react-ts/src/awsGraphqlFetch.ts
+++ frontend-react-ts/src/awsGraphqlFetch.ts
@@ -1,45 +1,43 @@
 import aws4 from "aws4-tiny";
-import { fetchAuthSession } from 'aws-amplify/auth';
+import { fetchAuthSession } from "aws-amplify/auth";
 
 export const awsGraphqlFetch = async (
-    uri: RequestInfo | URL,
-    options?: RequestInit| undefined,
-  ): Promise<Response> => {
-    const { host, path, graphqlPath, region } = {
-        host: process.env.REACT_APP_BACKEND_HOST,
-        path: process.env.REACT_APP_BACKEND_PATH,
-        graphqlPath: "/graphql",
-        region: process.env.REACT_APP_AWS_PROJECT_REGION,
-      };;
-
-    const request = {
-      host,
-      method: "POST",
-      path: `${path}${graphqlPath}`,
-      region,
-      service: "execute-api",
-      // NOTE: options.body is a json string
-      body: options?.body,
-      headers: options?.headers,
-    };
-
-    try {
-        const {
-            credentials,
-        } = await fetchAuthSession();
-    
-        const signedRequest = aws4.sign(request, credentials);
-        if (signedRequest?.headers && signedRequest?.headers["Host"]) {
-          delete signedRequest.headers["Host"];
-        }
-        if (signedRequest?.headers && signedRequest?.headers["Content-Length"]) {
-          delete signedRequest.headers["Content-Length"];
-        }
-        return fetch(uri, signedRequest as RequestInit);
-    } catch(e) {
-        if ((e as any).name as string === 'UserUnAuthenticatedException') {
-          throw Error('Unatuthenticated')
-        }
-        throw e
-      }
+  uri: RequestInfo | URL,
+  options?: RequestInit | undefined
+): Promise<Response> => {
+  const { host, path, graphqlPath, region } = {
+    host: process.env.REACT_APP_BACKEND_HOST,
+    path: process.env.REACT_APP_BACKEND_PATH,
+    graphqlPath: "/backend-apollo",
+    region: process.env.REACT_APP_AWS_PROJECT_REGION,
   };
+
+  const request = {
+    host,
+    method: "POST",
+    path: `${path}${graphqlPath}`,
+    region,
+    service: "execute-api",
+    // NOTE: options.body is a json string
+    body: options?.body,
+    headers: options?.headers,
+  };
+
+  try {
+    const { credentials } = await fetchAuthSession();
+
+    const signedRequest = aws4.sign(request, credentials);
+    if (signedRequest?.headers && signedRequest?.headers["Host"]) {
+      delete signedRequest.headers["Host"];
+    }
+    if (signedRequest?.headers && signedRequest?.headers["Content-Length"]) {
+      delete signedRequest.headers["Content-Length"];
+    }
+    return fetch(uri, signedRequest as RequestInit);
+  } catch (e) {
+    if (((e as any).name as string) === "UserUnAuthenticatedException") {
+      throw Error("Unatuthenticated");
+    }
+    throw e;
+  }
+};
